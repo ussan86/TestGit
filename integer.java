21249387fa548173155f98ba5a0a97177cdb326e
ussan86 <ussan86@gmail.com>
2019-05-12 13:52:18 +0530

    this is new

--- integer.java
+++ integer.java
@@ -2,7 +2,7 @@ class Integers {
       public static void main(String[] arguments) {
         int c; //declaring a variable
      
-      /* Using for loop to repeat instruction execution */
+      /* Using for loop to repeat instructions execution */
      
         for (c = 1; c <= 10; c++) {
           System.out.println(c);
